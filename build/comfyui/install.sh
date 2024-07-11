44e5471ac10f4d48d8b5816d8f8ffa8a476f04e7
bean980310 <bean980310@hotmail.co.kr>
2024-07-12 08:57:41 +0900

    new files and file updated

---
+++ build/comfyui/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# Clone the repo
+git clone https://github.com/comfyanonymous/ComfyUI.git /ComfyUI
+cd /ComfyUI
+git checkout ${COMFYUI_COMMIT}
+
+# Install requirements
+pip3 install -r requirements.txt
