c4e63221be66b194a39e4dca2f185e2ec08a4cb8
bean980310 <bean980310@hotmail.co.kr>
2024-07-12 18:10:10 +0900

    files updated

--- build/comfyui/install.sh
+++ build/comfyui/install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+# Clone the git repo of the Stable Diffusion Web UI by Automatic1111
+# and set version
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+cd /stable-diffusion-webui
+git checkout tags/${WEBUI_VERSION}
+
 # Clone the repo
 git clone https://github.com/comfyanonymous/ComfyUI.git /ComfyUI
 cd /ComfyUI
