8a0c3092f8893a8bf95452da75da6f905b00a95c
bean980310 <bean980310@hotmail.co.kr>
2024-07-13 08:44:21 +0900

    update install_custom_nodes.sh

--- workspace/ComfyUI/install_custom_nodes.sh
+++ workspace/ComfyUI/install_custom_nodes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir ./custom_nodes
+
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
 git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack.git custom_nodes/ComfyUI-Impact-Pack
 git clone https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git custom_nodes/ComfyUI-Inspire-Pack
