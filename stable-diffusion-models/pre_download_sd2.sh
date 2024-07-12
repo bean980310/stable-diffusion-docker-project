c8a250e57e965d7da15ceb1a625b7b7aacdaf222
bean980310 <bean980310@hotmail.co.kr>
2024-07-12 18:21:15 +0900

    file updated

--- stable-diffusion-models/pre_download_sd2.sh
+++ stable-diffusion-models/pre_download_sd2.sh
@@ -2,8 +2,8 @@
 
 mkdir sd-models/Stable-diffusion/SD2
 
-echo "Downloading SD 2.0 model"
-wget -P sd-models/Stable-diffusion/SD2 -i ./download_link/download_ckpt_sd20.txt
+# echo "Downloading SD 2.0 model"
+# wget -P sd-models/Stable-diffusion/SD2 -i ./download_link/download_ckpt_sd20.txt
 
 echo "Downloading SD 2.1 model"
 wget -P sd-models/Stable-diffusion/SD2 -i ./download_link/download_ckpt_sd21.txt
