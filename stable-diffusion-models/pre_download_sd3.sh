8e8b09d61374403dd30b15e29faa069de2ad36bf
bean980310 <bean980310@hotmail.co.kr>
2024-07-12 18:16:55 +0900

    file updated

--- stable-diffusion-models/pre_download_sd3.sh
+++ stable-diffusion-models/pre_download_sd3.sh
@@ -10,7 +10,7 @@ echo "Downloading SD 3 model"
 
 # cd /workspace/stable-diffusion-webui/models/Stable-diffusion
 
-mkdir sd-models/Stable-diffusion/SD3
+# mkdir sd-models/Stable-diffusion/SD3
 
 huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium.safetensors --local-dir=./sd-models/Stable-diffusion/SD3
 huggingface-cli download stabilityai/stable-diffusion-3-medium sd3_medium_incl_clips.safetensors --local-dir=./sd-models/Stable-diffusion/SD3
