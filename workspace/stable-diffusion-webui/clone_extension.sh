99294b94aa966e0b33315c32a7b7b527010fe069
bean980310 <bean980310@hotmail.co.kr>
2024-07-12 21:51:50 +0900

    added clone_extension.sh

---
+++ workspace/stable-diffusion-webui/clone_extension.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/d8ahazard/sd_dreambooth_extension.git extensions/sd_dreambooth_extension
+git clone https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
+git clone --depth=1 https://github.com/deforum-art/sd-webui-deforum.git extensions/deforum
+git clone --depth=1 https://github.com/ashleykleynhans/a1111-sd-webui-locon.git extensions/a1111-sd-webui-locon
+git clone --depth=1 https://github.com/Gourieff/sd-webui-reactor.git extensions/sd-webui-reactor
+git clone --depth=1 https://github.com/zanllp/sd-webui-infinite-image-browsing.git extensions/infinite-image-browsing
+git clone --depth=1 https://github.com/Uminosachi/sd-webui-inpaint-anything.git extensions/inpaint-anything
+git clone --depth=1 https://github.com/Bing-su/adetailer.git extensions/adetailer
+git clone --depth=1 https://github.com/civitai/sd_civitai_extension.git extensions/sd_civitai_extension
+git clone https://github.com/BlafKing/sd-civitai-browser-plus.git extensions/sd-civitai-browser-plus
+git clone https://github.com/NVIDIA/Stable-Diffusion-WebUI-TensorRT.git extensions/Stable-Diffusion-WebUI-TensorRT
+git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git extensions/tag-autocomplete
+git clone https://github.com/pharmapsychotic/clip-interrogator-ext.git extensions/clip-interrogator-ext
+git clone https://github.com/feynlee/latent-upscale.git extensions/latent-upscale
+git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git extensions/multidiffusion-upscaler-for-automatic1111
+git clone https://github.com/adieyal/sd-dynamic-prompts.git extensions/sd-dynamic-prompts
+git clone --depth=1 https://github.com/mcmonkeyprojects/sd-dynamic-thresholding extensions/sd-dynamic-thresholding
+git clone https://github.com/kohya-ss/sd-webui-additional-networks.git extensions/sd-webui-additional-networks
+git clone https://github.com/NON906/sd-webui-chatgpt.git extensions/sd-webui-chatgpt
+git clone https://github.com/hako-mikan/sd-webui-lora-block-weight.git extensions/sd-webui-lora-block-weight
+git clone https://github.com/hako-mikan/sd-webui-regional-prompter.git extensions/sd-webui-regional-prompter
+git clone https://github.com/butaixianran/Stable-Diffusion-Webui-Civitai-Helper.git extensions/Stable-Diffusion-Webui-Civitai-Helper
+git clone https://github.com/opparco/stable-diffusion-webui-composable-lora.git extensions/stable-diffusion-webui-composable-lora
+git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git extensions/ultimate-upscale-for-automatic1111
+git clone https://github.com/continue-revolution/sd-webui-animatediff.git extensions/sd-webui-animatediff
+git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git extensions/stable-diffusion-webui-images-browser
+git clone https://github.com/camenduru/stable-diffusion-webui-huggingface.git extensions/stable-diffusion-webui-huggingface
+git clone https://github.com/KutsuyaYuki/ABG_extension.git extensions/ABG_extension
+git clone https://github.com/Iyashinouta/sd-model-downloader.git extensions/sd-model-downloader
