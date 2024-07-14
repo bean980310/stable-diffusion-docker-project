5be130d5fda1c796a8fd7f46800157e4e95d73cc
bean980310 <bean980310@hotmail.co.kr>
2024-07-15 08:16:25 +0900

    Update start.sh

--- build/vladmandic-sdnext/start.sh
+++ build/vladmandic-sdnext/start.sh
@@ -4,4 +4,4 @@ set -e
 export HF_HOME="/app"
 
 cd /app/automatic
-nohup python3 launch.py --listen --port 7860 --skip-env> /app/sdnext.log 2>&1 &
+nohup python3 launch.py --listen --port 7860 > /app/sdnext.log 2>&1 &
