f180cb57e06586c3abf8016d5001a7a1278fc46d
bean980310 <bean980310@hotmail.co.kr>
2024-07-15 15:44:56 +0900

    Update run_first.sh

--- stable-diffusion-models/run_first.sh
+++ stable-diffusion-models/run_first.sh
@@ -1,38 +1,31 @@
-mkdir sd-models
-
-mkdir -vp sd-models/Stable-diffusion \
-    sd-models/Stable-diffusion/SD15 \
+mkdir -vp sd-models/Stable-diffusion/SD15 \
     sd-models/Stable-diffusion/SDXL \
     sd-models/Stable-diffusion/Pony \
     sd-models/Stable-diffusion/SD3 \
     sd-models/Stable-diffusion/upscale
 
-mkdir -vp sd-models/VAE \
-    sd-models/VAE/SD15 \
+mkdir -vp sd-models/VAE/SD15 \
     sd-models/VAE/SDXL \
     sd-models/VAE/SD3
 
-mkdir -vp sd-models/ControlNet \
-    sd-models/ControlNet/SD15 \
+mkdir -vp sd-models/ControlNet/SD15 \
     sd-models/ControlNet/SDXL \
     sd-models/ControlNet/SD3
 
-mkdir -vp sd-models/Lora \
-    sd-models/Lora/SD15 \
+mkdir -vp sd-models/Lora/SD15 \
     sd-models/Lora/SDXL \
     sd-models/Lora/Pony \
     sd-models/Lora/SD3
 
-mkdir -vp embeddings \
-    embeddings/SD15 \
+mkdir -vp embeddings/SD15 \
     embeddings/SDXL \
     embeddings/SD3
 
-mkdir -vp sd-models/ESRGAN \
-    sd-models/RealESRGAN \
-    sd-models/GFPGAN \
-    sd-models/LDSR
+# mkdir -vp sd-models/ESRGAN \
+#     sd-models/RealESRGAN \
+#     sd-models/GFPGAN \
+#     sd-models/LDSR
 
-mkdir sd-models/adetailer
-mkdir sd-models/sam
-mkdir sd-models/clip-interrogator
+# mkdir sd-models/adetailer
+# mkdir sd-models/sam
+# mkdir sd-models/clip-interrogator
