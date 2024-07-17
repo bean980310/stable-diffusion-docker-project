53bc17842fc294614da6c25a9ec871d42dfe9a0e
bean980310 <bean980310@hotmail.co.kr>
2024-07-17 12:08:24 +0900

    Add SwarmUI

---
+++ build/swarmui/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh --channel 7.0
+./dotnet-install.sh --channel 8.0
+
+git clone https://github.com/mcmonkeyprojects/SwarmUI.git
+cd /app/SwarmUI
+git checkout ${SWARMUI_VERSION}
+
+mkdir dlbackend
+
+cd /app/SwarmUI/dlbackend
+git clone https://github.com/comfyanonymous/ComfyUI.git
+cd /app/SwarmUI/dlbackend/ComfyUI
+git checkout ${COMFYUI_VERSION}
+
+pip3 install -r requirements.txt
---
+++ build/swarmui/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+export HF_HOME="/app"
+export SWARM_NO_VENV='true'
+
+cd /app/SwarmUI
+./launch-linux.sh
