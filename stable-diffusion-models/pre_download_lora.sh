d36f4d4368c81b2938869a9598d7593953a1fa81
bean980310 <bean980310@hotmail.co.kr>
2024-07-19 09:28:20 +0900

    Add download lora

--- stable-diffusion-models/pre_download_lora.sh
+++ stable-diffusion-models/pre_download_lora.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Downloading SD 1.5 Lora"
+echo "Downloading SD 1.5 LoRA"
 wget -P models/loras/sd15 --content-disposition -i ./download_link/download_lora_sd15.txt
 
-echo "Downloading SD XL Lora"
+echo "Downloading SD XL LoRA"
 wget -P models/loras/sdxl --content-disposition -i ./download_link/download_lora_sdxl.txt
 
-echo "Downloading Pony Lora"
+echo "Downloading Pony LoRA"
 wget -P models/loras/pony --content-disposition -i ./download_link/download_lora_pony.txt
