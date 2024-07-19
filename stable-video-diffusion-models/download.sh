c211a55d11db423fdbd64bf6a1cbc325e247df24
bean980310 <bean980310@hotmail.co.kr>
2024-07-19 16:57:59 +0900

    Update 4 files

--- stable-video-diffusion-models/download.sh
+++ stable-video-diffusion-models/download.sh
@@ -6,4 +6,4 @@ huggingface-cli login --token
 
 echo "Downloading SVD model"
 
-huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt-1-1 svd_xt_1_1.safetensors --local-dir=./models/checkpoints/svd
+huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt-1-1 svd_xt_1_1.safetensors --local-dir=./models
