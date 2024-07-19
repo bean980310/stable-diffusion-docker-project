efb5b43c4d20246e71cb443dafcc4fd86b8fec39
bean980310 <bean980310@hotmail.co.kr>
2024-07-19 09:19:54 +0900

    Add download svd

---
+++ stable-diffusion-models/pre_download_svd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Login to huggingface"
+
+huggingface-cli login --token
+
+echo "Downloading SVD model"
+
+huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt-1-1 svd_xt_1_1.safetensors --local-dir=./models/checkpoints/svd
