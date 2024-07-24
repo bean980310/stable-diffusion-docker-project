aae5ac393f549d29615be493539eb085159991b5
bean980310 <bean980310@hotmail.co.kr>
2024-07-25 08:09:23 +0900

    Update download.sh

--- stable-video-diffusion-models/download.sh
+++ stable-video-diffusion-models/download.sh
@@ -9,3 +9,5 @@ echo "Downloading SVD model"
 huggingface-cli download stabilityai/stable-video-diffusion-img2vid svd.safetensors --local-dir=./models
 huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt svd_xt.safetensors --local-dir=./models
 huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt-1-1 svd_xt_1_1.safetensors --local-dir=./models
+huggingface-cli download stabilityai/stable-video-diffusion-img2vid svd_image_decoder.safetensors --local-dir=./models
+huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt svd_xt_image_decoder.safetensors --local-dir=./models
