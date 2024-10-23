d93f8805d4e8e9455dd47c00aa3a732d8b4418a5
bean980310 <bean980310@hotmail.co.kr>
2024-10-23 19:18:25 +0900

    Add LLM download and sv3d and sv4d

--- stable-diffusion-models/start.sh
+++ stable-diffusion-models/start.sh
@@ -2,11 +2,11 @@
 set -e
 
 ./run_first.sh
-./pre_download.sh
-./pre_download_alt.sh
-./pre_download_sd2.sh
-./pre_download_sd3.sh
-./pre_download_flux1.sh
-./pre_download_controlnet.sh
-./pre_download_controlnet_sd2.sh
-./pre_download_lora.sh
+./download.sh
+./download_alt.sh
+./download_sd2.sh
+./download_sd3.sh
+./download_flux1.sh
+./download_controlnet.sh
+./download_controlnet_sd2.sh
+./download_lora.sh
--- stable-video-diffusion-models/download.sh
+++ stable-video-diffusion-models/download.sh
@@ -11,3 +11,6 @@ huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt svd_xt.sa
 huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt-1-1 svd_xt_1_1.safetensors --local-dir=./models
 huggingface-cli download stabilityai/stable-video-diffusion-img2vid svd_image_decoder.safetensors --local-dir=./models
 huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt svd_xt_image_decoder.safetensors --local-dir=./models
+huggingface-cli download stabilityai/sv3d sv3d_p.safetensors --local-dir=./models
+huggingface-cli download stabilityai/sv3d sv3d_u.safetensors --local-dir=./models
+huggingface-cli download stabilityai/sv4d sv4d.safetensors --local-dir=./models
