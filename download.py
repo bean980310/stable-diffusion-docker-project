890ea3aaa535cfa6a6fa0cf4cc80108ec69a33a8
bean980310 <bean980310@hotmail.co.kr>
2024-10-25 17:50:35 +0900

    Update 3 files

---
+++ download.py
@@ -0,0 +1,112 @@
+import os.path
+import sys
+import argparse
+import time
+import urllib.request
+from pathlib import Path
+from urllib.parse import urlparse, parse_qs, unquote
+import wget
+
+TOKEN_FILE = Path.home() / '.civitai' / 'config'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='CivitAI Downloader',
+    )
+
+    parser.add_argument(
+        'url',
+        type=str,
+        help='CivitAI Download URL, eg: https://civitai.com/api/download/models/46846'
+    )
+
+    parser.add_argument(
+        'output_path',
+        type=str,
+        help='Output path, eg: /workspace/stable-diffusion-webui/models/Stable-diffusion'
+    )
+
+    return parser.parse_args()
+
+def get_token():
+    try:
+        with open(TOKEN_FILE, 'r') as file:
+            token = file.read()
+            return token
+    except Exception as e:
+        return None
+
+def store_token(token: str):
+    TOKEN_FILE.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(TOKEN_FILE, 'w') as file:
+        file.write(token)
+
+def prompt_for_civitai_token():
+    token = input('Please enter your CivitAI API token: ')
+    store_token(token)
+    return token
+
+def download(url, out_path):
+    if not os.path.exists(out_path):
+        os.makedirs(out_path, exist_ok=True)
+    wget.download(url, out=out_path)
+
+def download_file(url: str, output_path: str, token: str):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'User-Agent': USER_AGENT,
+    }
+
+    class NoRedirection(urllib.request.HTTPErrorProcessor):
+        def http_response(self, request, response):
+            return response
+        https_response = http_response
+
+    request = urllib.request.Request(url, headers=headers)
+    opener = urllib.request.build_opener(NoRedirection)
+    response = opener.open(request)
+
+    if response.status in [301, 302, 303, 307, 308]:
+        redirect_url = response.getheader('Location')
+
+        # Extract filename from the redirect URL
+        parsed_url = urlparse(redirect_url)
+        query_params = parse_qs(parsed_url.query)
+        content_disposition = query_params.get('response-content-disposition', [None])[0]
+
+        if content_disposition:
+            filename = unquote(content_disposition.split('filename=')[1].strip('"'))
+        else:
+            raise Exception('Unable to determine filename')
+
+        response = urllib.request.urlopen(redirect_url)
+    elif response.status == 404:
+        raise Exception('File not found')
+    else:
+        raise Exception('No redirect found, something went wrong')
+
+    total_size = response.getheader('Content-Length')
+
+    if total_size is not None:
+        total_size = int(total_size)
+
+    # output_file = os.path.join(output_path, filename)
+
+    if not os.path.exists(filename):
+        download(url, output_path)
+        print(f'File downloaded: {filename}')
+    else:
+        print(f'File already exists: {filename}')
+
+args=get_args()
+token=get_token()
+        
+if not token:
+    token = prompt_for_civitai_token()
+
+try:
+    download_file(args.url, args.output_path, token)
+except Exception as e:
+    print(f'ERROR: {e}')
