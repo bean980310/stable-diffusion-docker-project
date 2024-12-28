5d6c73521d9b8bbf342db7bfc790b8bba60c9827
bean980310 <bean980310@hotmail.co.kr>
2024-12-29 00:34:51 +0900

    Update webui-user.sh

--- workspace/stable-diffusion-webui/webui-user.sh
+++ workspace/stable-diffusion-webui/webui-user.sh
@@ -19,7 +19,7 @@ export COMMANDLINE_ARGS="--port 7860 --listen --api --xformers --enable-insecure
 #export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-# venv_dir="venv"
+venv_dir="-"
 
 # script to launch to start the app
 #export LAUNCH_SCRIPT="launch.py"
