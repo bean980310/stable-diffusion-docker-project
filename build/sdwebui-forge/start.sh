44a3dab1f37c084c83ab2c5f73eeef63461ad5dd
bean980310 <bean980310@hotmail.co.kr>
2024-12-28 18:58:15 +0900

    Delete 6 files.

--- build/sdwebui-forge/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# export HF_HOME="/app"
-
-cd /app/stable-diffusion-webui-forge
-./webui.sh -f
--- build/vladmandic-sdnext/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# export HF_HOME="/app"
-
-cd /app/automatic
-python3 launch.py --listen --port 7860
