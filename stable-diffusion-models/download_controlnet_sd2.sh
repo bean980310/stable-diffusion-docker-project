b669e33ce08c0e6aeaf59508161984339c3ecccc
bean980310 <bean980310@hotmail.co.kr>
2024-12-30 17:27:21 +0900

    Delete 41 models

--- stable-diffusion-models/download_controlnet_sd2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Downloading ControlNet model for SD 2.1"
-wget -P models/controlnet/sd2 -i ./download_link/download_controlnet_sd2.txt
--- stable-diffusion-models/download_sd2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# echo "Downloading SD 2.0 model"
-# wget -P models/checkpoints/sd2 -i ./download_link/download_ckpt_sd2.txt
-
-echo "Downloading SD 2.1 model"
-wget -P models/checkpoints/sd2 -i ./download_link/download_ckpt_sd21.txt
