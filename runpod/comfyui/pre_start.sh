c8d6f40a595e9b80eb0410daa8db2921c8567a16
bean980310 <bean980310@hotmail.co.kr>
2024-12-31 16:54:22 +0900

    Ready for repair runpod images

--- runpod/comfyui/pre_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# export PYTHONUNBUFFERED=1
-# source /venv/bin/activate
-# rsync -au --remove-source-files /ComfyUI/ /workspace/ComfyUI/
-# ln -s /comfy-models/* /workspace/ComfyUI/models/checkpoints/
-
-cd /app/ComfyUI
-python main.py --listen --port 8188 &
