9d0581548c23978ab4e372296b7cd8e2b2e2cda1
bean980310 <bean980310@hotmail.co.kr>
2024-12-31 07:20:07 +0000

    Update start.sh

--- container-template/start.sh
+++ container-template/start.sh
@@ -76,7 +76,7 @@ start_jupyter() {
         echo "Starting Jupyter Lab..."
         mkdir -p /app && \
         cd / && \
-        nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
+        nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/app &> /jupyter.log &
         echo "Jupyter Lab started"
     fi
 }
