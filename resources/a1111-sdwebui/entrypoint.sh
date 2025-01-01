c377c852f9f0dccfb39f384ac4bfc527ca86d502
bean980310 <bean980310@hotmail.co.kr>
2025-01-01 18:11:01 +0900

    Move resources

--- resources/a1111-sdwebui/entrypoint.sh
+++ resources/a1111-sdwebui/entrypoint.sh
@@ -64,6 +64,8 @@ export_env_vars() {
 start_jupyter() {
     if [[ $JUPYTER_PASSWORD ]]; then
         echo "Starting Jupyter Lab..."
+        mkdir -p /app && \
+        cd / && \
         nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/app &> /jupyter.log &
         echo "Jupyter Lab started"
     fi
